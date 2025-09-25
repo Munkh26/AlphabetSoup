f435f256175c714a0fa775b636a4349596c0fe81
Munkh26 <munkhbatmunkhsoyombo@gmail.com>
2025-09-25 22:20:10 +0000

    updated

--- Soup.java
+++ Soup.java
@@ -33,12 +33,16 @@ public class Soup {
 
     //adds a word to the pool of letters known as "letters"
     public void add(String word){
+        //precondition: place anything after add
+        //poscondition: returns the letters with added words
         letters += word;
     }
 
 
     //Use Math.random() to get a random character from the letters string and return it.
     public char randomLetter(){
+        //precondition: must have added some value into the letters then use randomLetter
+        //poscondition: returns a random char from the letters
         int num = letters.length();
         int randomNum = (int) (Math.random() * num); 
         char randomCharacter = letters.charAt(randomNum);
@@ -49,6 +53,8 @@ public class Soup {
     //returns the letters currently stored with the company name placed directly in the center of all
     //the letters
     public String companyCentered(){
+        //precondition: must have value in the letters, and add the company name
+        //poscondition: returns first half of letters then company name then second half of letters 
         int num1 = (letters.length() / 2);
         System.out.println(num1);
         String currentLetter = letters.substring(0, num1);
@@ -64,16 +70,23 @@ public class Soup {
         //int test3 = letters.indexOf("i");
         //int test4 = letters.indexOf("o");
         //int test5 = letters.indexOf("u");
-        //Fix this later
+        
     }
 
     //should remove "num" letters from a random spot in the string letters. You may assume num never exceeds the length of the string.
     public void removeSome(int num){
-
+        //precondition: User input never exceeds the length of the Letters
+        //poscondition: it returns letters that got removed num letters from a random spot
+        int randomIndex = (int) (Math.random() * letters.length());
+        String part1 = letters.substring(0, randomIndex);
+        int num2 = num - randomIndex;
+        String part2 = letters.substring(randomIndex + num2);
+        letters = part1 + part2;
     }
 
     //should remove the word "word" from the string letters. If the word is not found in letters then it does nothing.
     public void removeWord(String word){
         
+
     }
 }
